7f91700d6739cc0ac61bcf3b97d44dc566d26343
gregsanz182 <sanchz93@gmail.com>
2018-03-25 13:43:50 -0400

    Acomodadas las excepciones de RequestHandler

--- RequestsHandler.py
+++ RequestsHandler.py
@@ -19,24 +19,54 @@ class RequestsHandler():
             if r.status_code == 200:
                 return r.json()
             if r.status_code == 404:
-                raise RequestsHandlerException("La URL del dominio no es válida.")
+                raise InvalidURL()
             if r.status_code == 401:
-                raise RequestsHandlerException("Credenciales incorrectas")
+                raise InvalidAuthetication()
             if r.status_code == 500:
-                raise RequestsHandlerException("Error interno del servidor (500)")
-            raise RequestsHandlerException("Ocurrió un error en la petición.\nCódigo HTTP: {}".format(r.status_code))
+                raise InternalServerError()
+            raise RequestError(r.status_code)
         except requests.exceptions.InvalidURL as e:
-            raise RequestsHandlerException("La URL del dominio no es válida.", e)
+            raise InvalidURL(e)
         except requests.exceptions.MissingSchema as e:
-            raise RequestsHandlerException("El dominio no es valido.\nVerifica que incluya 'http://' o 'https://'", e)
+            raise MissingSchema(e)
         except requests.exceptions.ConnectionError as e:
-            raise RequestsHandlerException("Error en la conexión.\nVerifica que exista conexión a internet y vuelve a intentarlo.", e)
+            raise GeneralConnectionError(e)
         except requests.exceptions.ConnectTimeout as e:
-            raise RequestsHandlerException("Error en la conexión.\nVerifica que exista conexión a internet y vuelve a intentarlo.", e)
+            raise GeneralConnectionError(e)
 
-class RequestsHandlerException(ValueError):
+class RequestsHandlerException(Exception):
 
-    def __init__(self, message, original_exception=None):
+    def __init__(self, message, original_exception = None):
         super().__init__(message)
         self.message = message
         self.original_exception = original_exception
+
+class InvalidURL(RequestsHandlerException):
+
+    def __init__(self, original_exception = None):
+        super().__init__("La URL del dominio no es válida.", original_exception)
+
+class InternalServerError(RequestsHandlerException):
+
+    def __init__(self, original_exception = None):
+        super().__init__("Error interno del servidor (500)", original_exception)
+
+class RequestError(RequestsHandlerException):
+
+    def __init__(self, http_code, original_exception = None):
+        super().__init__("Ocurrió un error en la petición.\nCódigo HTTP: {}".format(http_code), original_exception)
+
+class InvalidAuthetication(RequestsHandlerException):
+
+    def __init__(self, original_exception = None):
+        super().__init__("Credenciales incorrectas.", original_exception)
+
+class MissingSchema(RequestsHandlerException):
+
+    def __init__(self, original_exception = None):
+        super().__init__("El dominio no es valido.\nVerifica que incluya 'http://' o 'https://'", original_exception)
+
+class GeneralConnectionError(RequestsHandlerException):
+
+    def __init__(self, original_exception = None):
+        super().__init__("Error en la conexión.\nVerifica que exista conexión a internet y vuelve a intentarlo.", original_exception)
